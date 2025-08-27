3b3d4c78cb695c972aa6512b12536c9940357a2e
Lucas Facina <2411600015@fema.edu.br>
2025-08-27 15:00:02 -0300

    refactor: ✏️ renomear váriaveis de coluna

--- presentation/pages/home.py
+++ presentation/pages/home.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from PIL import Image
 
-col1, col2, col3 = st.columns((1, 4, 1))
-with col2:
+col_left, col_middle, col_right = st.columns((1, 4, 1))
+with col_middle:
     st.image(Image.open("assets/img/home_logo.png"))
 
 st.markdown("# Bem-vindo(a) ao Sistema Synapse!")
