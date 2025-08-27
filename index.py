6daceabcc85f00e20d94fbeb48c501bf950c4b98
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-08-27 13:11:46 -0300

    feat: ✨ adicionar configuração de logo e ícone da página no `index.py`

--- index.py
+++ index.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.logo("assets/img/sidebar_logo.png", icon_image="assets/img/main_body_logo.png")
+
+st.set_page_config(
+    page_icon="assets/img/favicon.png",
+)
+
 app = st.navigation({
     "Início": [
         st.Page("presentation/pages/home.py", title="Bem-vindo(a)", icon=":material/home:")
