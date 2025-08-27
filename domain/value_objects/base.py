94c03980e3a1b7ed73ffdcc85adc31257d5847ca
Lucas Facina <2411600015@fema.edu.br>
2025-08-27 16:04:50 -0300

    feat: ✨ adicionar base para `ValueObjects`

---
+++ domain/value_objects/base.py
@@ -0,0 +1,17 @@
+from typing import TypeVar, Generic
+from pydantic import BaseModel, ConfigDict, field_validator
+
+T = TypeVar('T')
+
+
+class ValueObject(BaseModel, Generic[T]):
+    model_config = ConfigDict(frozen=True)
+
+    value: T
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+
+class CompositeValueObject(BaseModel):
+    model_config = ConfigDict(frozen=True)
