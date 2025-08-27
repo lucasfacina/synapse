ee890ff706ac07551521712a464f1717d01b4675
Thiago Ausechi <thiago_ausechi@icloud.com>
2025-08-27 13:02:43 -0300

    refactor: 💄 adicionar imagem de logo na página inicial

--- presentation/pages/home.py
+++ presentation/pages/home.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from PIL import Image
 
-st.write("# Bem-vindo(a) ao Sistema Synapse!")
+col1, col2, col3 = st.columns((1, 4, 1))
+with col2:
+    st.image(Image.open("assets/img/home_logo.png"))
+
+st.markdown("# Bem-vindo(a) ao Sistema Synapse!")
